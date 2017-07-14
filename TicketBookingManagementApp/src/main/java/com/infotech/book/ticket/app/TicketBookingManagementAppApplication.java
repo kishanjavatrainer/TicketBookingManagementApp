e0293be4ff567559f58bb4e451bad224b7adf513
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-07-15 00:45:45 +0530

    TicketBookingManagementApp Source code

---
+++ TicketBookingManagementApp/src/main/java/com/infotech/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -0,0 +1,29 @@
+package com.infotech.book.ticket.app;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.infotech.book.ticket.app.entities.Ticket;
+import com.infotech.book.ticket.app.service.TicketBookingService;
+
+@SpringBootApplication
+public class TicketBookingManagementAppApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(TicketBookingManagementAppApplication.class, args);
+		
+		TicketBookingService ticketBookingService = applicationContext.getBean("ticketBookingService", TicketBookingService.class);
+		
+		Ticket ticket = new Ticket();
+		ticket.setBookingDate(new Date());
+		ticket.setDestStation("Mumbai");
+		ticket.setSourceStation("Pune");
+		ticket.setPassengerName("KK");
+		ticket.setEmail("kk@yahoo.com");
+		
+		ticketBookingService.createTicket(ticket);
+	}
+}
---
+++ TicketBookingManagementApp/src/main/java/com/infotech/book/ticket/app/controller/TicketBookingController.java
@@ -0,0 +1,47 @@
+package com.infotech.book.ticket.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infotech.book.ticket.app.entities.Ticket;
+import com.infotech.book.ticket.app.service.TicketBookingService;
+
+@RestController
+@RequestMapping(value="/api/tickets")
+public class TicketBookingController {
+
+	@Autowired
+	private TicketBookingService ticketBookingService;
+	
+	@PostMapping(value="/create")
+	public Ticket createTicket(@RequestBody Ticket ticket){
+		return ticketBookingService.createTicket(ticket);
+	}
+	
+	@GetMapping(value="/ticket/{ticketId}")
+	//@RequestMapping(value="/ticket/{ticketId}")
+	public Ticket getTicketById(@PathVariable("ticketId")Integer ticketId){
+		return ticketBookingService.getTicketById(ticketId);
+	}
+	@GetMapping(value="/ticket/alltickets")
+	public Iterable<Ticket> getAllBookedTickets(){
+		return ticketBookingService.getAllBookedTickets();
+	}
+	
+	@DeleteMapping(value="/ticket/{ticketId}")
+	public void deleteTicket(@PathVariable("ticketId")Integer ticketId){
+		ticketBookingService.deleteTicket(ticketId);
+	}
+	
+	@PutMapping(value="/ticket/{ticketId}/{newEmail:.+}")
+	public Ticket updateTicket(@PathVariable("ticketId")Integer ticketId,@PathVariable("newEmail")String newEmail){
+		return ticketBookingService.updateTicket(ticketId,newEmail);
+	}
+}
